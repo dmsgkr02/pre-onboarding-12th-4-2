20a22af6f379c71db8fc6b47adc46ef9fff3483b
dmsgkr02 <dmsgkr02@naver.com>
2023-08-31 17:41:27 +0900

    Fix eslint error

--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router'
 import { useAppSelector } from '../redux/hook';
