67c5ca764d21c4d033cf19c9e62dad0ddef8e5b9
dmsgkr02 <dmsgkr02@naver.com>
2023-09-01 19:21:44 +0900

    Fix IssueList

--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { fetchIssueList } from "../redux/slices/issue";
 import { useScroll } from "../hooks/useScroll";
@@ -23,39 +23,41 @@ export default function IssueList() {
   } = useAppSelector((state) => state.issuesList);
   const { scrollHeight, scrollY } = useScroll();
 
-  // useEffect(() => {
-  //   issueList.length === 0 && dispatch(fetchIssueList(1));
-  // }, [issueList.length, dispatch]);
-
-  // useEffect(() => {
-  //   if (loading || scrollY === scrollHeight || !hasMore) return;
-  //   if (scrollY > scrollHeight - scrollOffsetHeight) {
-  //     dispatch(fetchIssueList(page + 1));
-  //     return;      
-  //   }
-  // }, [loading, page, issueList, scrollY, scrollHeight, issueList, dispatch, hasMore]);
-
-  const nextIssueDispatch = useCallback(() => {
-    if (loading || !hasMore) return;
-    dispatch(
-      fetchIssueList(page + 1),
-    );
-  }, [dispatch, hasMore, loading, page]);
-
   useEffect(() => {
-    !issueList.length &&
-      dispatch(
-        fetchIssueList(page),
-      );
+    issueList.length === 0 && dispatch(fetchIssueList(1));
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    if (scrollY === scrollHeight) return;
+    if (loading || scrollY === scrollHeight || !hasMore) return;
     if (scrollY > scrollHeight - scrollOffsetHeight) {
-      nextIssueDispatch();
-      return;
+      dispatch(fetchIssueList(page + 1));
+      return;      
     }
-  }, [scrollY, scrollHeight, nextIssueDispatch]);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loading, page, issueList, scrollY, scrollHeight, issueList, hasMore]);
+
+  // const nextIssueDispatch = useCallback(() => {
+  //   if (loading || !hasMore) return;
+  //   dispatch(
+  //     fetchIssueList(page + 1),
+  //   );
+  // }, [dispatch, hasMore, loading, page]);
+
+  // useEffect(() => {
+  //   !issueList.length &&
+  //     dispatch(
+  //       fetchIssueList(page),
+  //     );
+  // }, []);
+
+  // useEffect(() => {
+  //   if (scrollY === scrollHeight) return;
+  //   if (scrollY > scrollHeight - scrollOffsetHeight) {
+  //     nextIssueDispatch();
+  //     return;
+  //   }
+  // }, [scrollY, scrollHeight, nextIssueDispatch]);
 
   if (error) {
     return <ErrorPage errorMesage={error} />;
